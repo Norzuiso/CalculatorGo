f6e3c07a293624928fd0aa96627b3b2fee4cbc2d
josue_aguilar <josueaguilarjnai@gmail.com>
2021-04-21 13:57:18 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	menu := `
+	Welcome!
+	[1] addition.
+	[2] subtraction.
+	[3] multiplication.
+	[4] division.
+	[5] exit.
+`
+	fmt.Println(menu)
+}
