e95e9badc071d5d10b16407b95e92bf9f0347b9a
josue_aguilar <josueaguilarjnai@gmail.com>
2021-04-23 11:39:31 -0500

    menu

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	showMenu()
+}
 
-	menu := `
+func showMenu() {
+	var option int
+	for option != 6 {
+		menu := `
 	Welcome!
 	[1] addition.
 	[2] subtraction.
 	[3] multiplication.
 	[4] division.
-	[5] exit.
-`
-	fmt.Println(menu)
+	[5] Rules and instructions.
+	[6] exit.`
+		fmt.Println(menu)
+		fmt.Scan(&option)
+		options(option)
+	}
+}
+
+func options(option int) {
+	switch option {
+	case 1:
+		fmt.Printf("Additon.")
+		break
+	case 2:
+		fmt.Printf("Substrction.")
+		break
+	case 3:
+		fmt.Printf("Multiplication.")
+		break
+	case 4:
+		fmt.Printf("Division.")
+		break
+	case 5:
+		fmt.Printf("Rules and instructions.")
+		break
+	case 6:
+		fmt.Printf("Exit")
+		break
+	}
 }
